7aa035b191f6dc7bccbe50ff5a268bd08527177f
Patrik Ackland <patrikackland@gmail.com>
2016-03-25 18:07:32 -0700

    Github login auth

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
 from flask import Flask, render_template, request,redirect
 import os
+from flask.ext.github import GitHub
+import json
 
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+fp = os.path.join(SITE_ROOT, 'config.json')
 app = Flask(__name__)
-
-print app.root_path
+global token
+token = None
+with open(fp) as cred:
+    creds = json.load(cred)
+app.config['GITHUB_CLIENT_ID'] = creds['id']
+app.config['GITHUB_CLIENT_SECRET'] = creds['secret']
+github = GitHub(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return github.authorize()
+
+@app.route('/callback')
+@github.authorized_handler
+def authorized(oauth_token):
+    global token
+    token = oauth_token
+    data = github.get('user')
+    return render_template('', data=data)
+
+@github.access_token_getter
+def token_getter():
+    return token
 
 if __name__ == "__main__":
     app.debug=True
